d568dd009b0f6683b2342642db485736ef59398f
Thomas Indrias <thomasindrias@gmail.com>
2019-10-06 02:40:22 +0200

    Simple layout

---
+++ lib/pages/page_3.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ThirdPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Third Page"),
+    );
+  }
+}
---
+++ lib/pages/page_4.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FourthPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Fourth Page"),
+    );
+  }
+}
